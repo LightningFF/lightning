98529e6010b35c7dd0cc00e1b6a97a7ac5e69cab
Ramruthwick Pathireddy <rpathire@caltech.edu>
2021-05-26 12:39:04 -0400

    Separate compilation from host application

--- lib/lightning/engine.rb
+++ lib/lightning/engine.rb
@@ -2,6 +2,10 @@ module Lightning
   class Engine < ::Rails::Engine
     isolate_namespace Lightning
 
+    initializer "lightning.assets.precompile" do |app|
+      app.config.assets.precompile += %w( lightning/application.css )
+    end
+
     config.generators do |g|
       g.test_framework :rspec
     end
