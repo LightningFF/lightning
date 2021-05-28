0503e9de9b16ee2d300c67fcbcd9ffc37bf14dae
Pranav Singh <hello@pranavsingh.me>
2021-05-28 13:40:40 +0100

    Allow to add multiple entities at the same time

--- app/controllers/lightning/feature_opt_ins_controller.rb
+++ app/controllers/lightning/feature_opt_ins_controller.rb
@@ -5,14 +5,21 @@ module Lightning
 
     def create
       @feature = Feature.find(params[:feature_id])
-      @feature_opt_in = @feature.feature_opt_ins.new
-      @feature_opt_in.entity_id = params[:feature_opt_in][:entity_id]
-      @feature_opt_in.entity_type = params[:feature_opt_in][:entity_type]
-      if @feature_opt_in.save
-        flash[:notice] = "Permissions has been created!"
-      else
-        flash[:notice] = "FAILED TO SAVE PERMISSIONS! INVALID ENTITY ID!"
+      entity_ids = params[:feature_opt_in][:entity_id].split(",")
+      entity_class = params[:feature_opt_in][:entity_type].constantize
+
+      entity_ids.each do |entity_id|
+        entity = entity_class.find(entity_id)
+        feature_opt_in = @feature.feature_opt_ins.new
+        feature_opt_in.entity_id = entity.id
+        feature_opt_in.entity_type = entity_class.to_s
+        feature_opt_in.save!
       end
+
+      flash[:notice] = "Permissions has been created!"
+      redirect_to @feature
+    rescue => e
+      flash[:notice] = "Failed to opt in some entities: #{e.message}"
       redirect_to @feature
     end
 
@@ -24,6 +31,7 @@ module Lightning
     end
 
     private
+
     def feature_opt_ins_params
       params.require(:feature_opt_in).permit(:entity_id, :entity_type)
     end
