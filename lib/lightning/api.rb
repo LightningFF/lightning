5e59e2d4d3155daf1a50f71cc9cad379b43ff89f
Ruthwick Pathireddy <ruthwickp@yahoo.com>
2021-06-04 12:49:01 -0400

    Downgrade rails dependency to 5.0.0 (#25)

--- lib/lightning/api.rb
+++ lib/lightning/api.rb
@@ -33,7 +33,7 @@ module Lightning
     end
 
     def self.opt_in(key, entity)
-      get(key).feature_opt_ins.create_or_find_by!(entity_id: entity.id, entity_type: entity.class.to_s)
+      get(key).feature_opt_ins.find_or_create_by!(entity_id: entity.id, entity_type: entity.class.to_s)
     end
 
     def self.opt_out(key, entity)
@@ -42,12 +42,8 @@ module Lightning
       raise ::Lightning::Errors::EntityNotFound, "Could not find entity with id #{entity.id} and type #{entity.class.to_s}"
     end
 
-    def self.enabled?(entity, feature_key)
-      joined_table = Feature.left_outer_joins(:feature_opt_ins)
-      joined_table
-        .where(key: feature_key, state: :enabled_per_entity, feature_opt_ins: { entity_id: entity.id, entity_type: entity.class.to_s })
-        .or(joined_table.where(key: feature_key, state: :enabled_globally))
-        .exists?
+    def self.enabled?(feature_key, entity)
+      Feature.where(key: feature_key).left_outer_joins(:feature_opt_ins).where("state = ? OR (state = ? AND lightning_feature_opt_ins.entity_id = ? AND lightning_feature_opt_ins.entity_type = ?)", Feature.states[:enabled_globally], Feature.states[:enabled_per_entity], entity.id, entity.class.to_s).exists?
     end
   end
 end
