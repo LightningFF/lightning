3634658ff0a3585558b6c550ca55163684270849
Pranav <pranav.singh@intercom.io>
2021-06-06 08:50:58 +0100

    Updates to the docs, and adding some safety nets for lightning_display helper (#26)

--- app/helpers/lightning/flaggable.rb
+++ app/helpers/lightning/flaggable.rb
@@ -1,7 +1,11 @@
 module Lightning
   module Flaggable
     def lightning_display
-      "#{self.id} - #{self.name}"
+      if self.respond_to?(:name)
+        "#{self.id} - #{self.name}"
+      else
+        self.id
+      end
     end
   end
 end
--- app/models/lightning/feature_opt_in.rb
+++ app/models/lightning/feature_opt_in.rb
@@ -7,7 +7,16 @@ module Lightning
     belongs_to :entity, polymorphic: true
     before_validation :set_entity
 
+    def entity_name
+      if entity.respond_to?(:lightning_display)
+        entity.lightning_display
+      else
+        entity.id
+      end
+    end
+
     private
+
     def set_entity
       self.entity = self.entity_type.constantize.find_by_id(self.entity_id)
     end
